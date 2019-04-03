369553be5f645981a22093dc654799e8cf96893a
faraday58 <jarodriguezvera@gmail.com>
2019-04-03 14:58:50 -0600

    Llamada

--- app/src/main/java/com/temas/selectos/contactos/ContactoAdaptador.java
+++ app/src/main/java/com/temas/selectos/contactos/ContactoAdaptador.java
@@ -1,10 +1,17 @@
 package com.temas.selectos.contactos;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,10 +20,12 @@ import java.util.ArrayList;
 public class ContactoAdaptador extends RecyclerView.Adapter <ContactoAdaptador.ContactoViewHolder>{
 
     ArrayList<Contacto> contactos;
+    Activity activity;
 
-    public ContactoAdaptador(ArrayList<Contacto> contactos)
+    public ContactoAdaptador(ArrayList<Contacto> contactos,Activity activity)
     {
         this.contactos = contactos;
+        this.activity = activity;
     }
 
 
@@ -29,11 +38,24 @@ public class ContactoAdaptador extends RecyclerView.Adapter <ContactoAdaptador.C
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ContactoViewHolder contactoViewHolder, int posicion) {
+    public void onBindViewHolder(@NonNull final ContactoViewHolder contactoViewHolder, int posicion) {
             final Contacto contacto = contactos.get(posicion);
             contactoViewHolder.txtv_correo.setText(contacto.getCorreo());
             contactoViewHolder.txtv_telefono.setText(contacto.getTelefono());
             contactoViewHolder.imgFoto.setImageResource(contacto.getIdFoto());
+            contactoViewHolder.imgbTelefono.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String telefono = contacto.getTelefono();
+                    if (ActivityCompat.checkSelfPermission(v.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        return;
+                    }
+                    activity.startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+telefono)));
+
+                }
+            });
+
 
 
     }
@@ -48,12 +70,16 @@ public class ContactoAdaptador extends RecyclerView.Adapter <ContactoAdaptador.C
         private ImageView imgFoto;
         private TextView txtv_correo;
         private TextView txtv_telefono;
+        private ImageButton imgbTelefono;
+        private ImageButton imgbCorreo;
 
         public ContactoViewHolder(@NonNull View itemView) {
             super(itemView);
             imgFoto = itemView.findViewById(R.id.imgFoto);
             txtv_telefono =itemView.findViewById(R.id.txtvTelefono);
             txtv_correo = itemView.findViewById(R.id.txtvCorreo);
+            imgbCorreo = itemView.findViewById(R.id.imgbCorreo);
+            imgbTelefono = itemView.findViewById(R.id.imgbTelefono);
         }
     }
 }
--- app/src/main/java/com/temas/selectos/contactos/MainActivity.java
+++ app/src/main/java/com/temas/selectos/contactos/MainActivity.java
@@ -2,6 +2,7 @@ package com.temas.selectos.contactos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -16,8 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rcListaContactos = findViewById(R.id.rcListaContactos);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        rcListaContactos.setLayoutManager(llm);
+        IniciarListaContactos();
+        iniciarAdaptador();
 
+    }
 
+    public void iniciarAdaptador(){
+        ContactoAdaptador adaptador = new ContactoAdaptador(contactos);
+        rcListaContactos.setAdapter(adaptador);
     }
 
     public void IniciarListaContactos()
