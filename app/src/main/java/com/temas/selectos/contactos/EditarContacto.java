bcf49a9c29dce9da692f1a168e0999fe2759de57
faraday58 <jarodriguezvera@gmail.com>
2019-04-10 16:42:27 -0500

    Paso de datos

--- app/src/main/java/com/temas/selectos/contactos/EditarContacto.java
+++ app/src/main/java/com/temas/selectos/contactos/EditarContacto.java
@@ -2,8 +2,13 @@ package com.temas.selectos.contactos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditarContacto extends AppCompatActivity {
+
+    EditText edtTelefono;
+    EditText edtCorreo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +17,13 @@ public class EditarContacto extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         String Telefono = extras.getString("Telefono");
         String Correo = extras.getString("Correo");
+
+        edtTelefono = findViewById(R.id.edtTelefono);
+        edtCorreo = findViewById(R.id.edtCorreo);
+
+        edtCorreo.setText(Correo);
+        edtTelefono.setText(Telefono);
+
 
     }
 }
